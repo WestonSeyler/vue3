7b4fa6b0bbf1b9f4cadead251d62e9bc2538fec0
wjn <jianing.wu@wowjoy.cn>
2024-01-04 15:46:36 +0800

    feat:vue3 响应式复盘

---
+++ packages/vue/dist/mini-vue.cjs.js
@@ -0,0 +1,323 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function createDep(effects) {
+    const dep = new Set(effects);
+    return dep;
+}
+
+var ShapeFlags;
+(function (ShapeFlags) {
+    ShapeFlags[ShapeFlags["ELEMENT"] = 1] = "ELEMENT";
+    ShapeFlags[ShapeFlags["STATEFUL_COMPONENT"] = 4] = "STATEFUL_COMPONENT";
+    ShapeFlags[ShapeFlags["TEXT_CHILDREN"] = 8] = "TEXT_CHILDREN";
+    ShapeFlags[ShapeFlags["ARRAY_CHILDREN"] = 16] = "ARRAY_CHILDREN";
+    ShapeFlags[ShapeFlags["SLOTS_CHILDREN"] = 32] = "SLOTS_CHILDREN";
+})(ShapeFlags || (ShapeFlags = {}));
+
+const isObject = (val) => {
+    return val !== null && typeof val === "object";
+};
+const extend = Object.assign;
+function hasChanged(value, oldValue) {
+    return !Object.is(value, oldValue);
+}
+
+let activeEffect = void 0;
+let shouldTrack = false;
+const targetMap = new WeakMap();
+class ReactiveEffect {
+    constructor(fn, scheduler) {
+        this.fn = fn;
+        this.scheduler = scheduler;
+        this.active = true;
+        this.deps = [];
+        console.log("创建 ReactiveEffect 对象");
+    }
+    run() {
+        console.log("run");
+        if (!this.active) {
+            return this.fn();
+        }
+        shouldTrack = true;
+        activeEffect = this;
+        console.log("执行用户传入的 fn");
+        const result = this.fn();
+        shouldTrack = false;
+        activeEffect = undefined;
+        return result;
+    }
+    stop() {
+        if (this.active) {
+            cleanupEffect(this);
+            if (this.onStop) {
+                this.onStop();
+            }
+            this.active = false;
+        }
+    }
+}
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep) => {
+        dep.delete(effect);
+    });
+    effect.deps.length = 0;
+}
+function effect(fn, options = {}) {
+    const _effect = new ReactiveEffect(fn);
+    extend(_effect, options);
+    _effect.run();
+    const runner = _effect.run.bind(_effect);
+    runner.effect = _effect;
+    return runner;
+}
+function stop(runner) {
+    runner.effect.stop();
+}
+function track(target, type, key) {
+    if (!isTracking()) {
+        return;
+    }
+    console.log(`触发 track -> target: ${target} type:${type} key:${key}`);
+    let depsMap = targetMap.get(target);
+    if (!depsMap) {
+        depsMap = new Map();
+        targetMap.set(target, depsMap);
+    }
+    let dep = depsMap.get(key);
+    if (!dep) {
+        dep = createDep();
+        depsMap.set(key, dep);
+    }
+    trackEffects(dep);
+}
+function trackEffects(dep) {
+    if (!dep.has(activeEffect)) {
+        dep.add(activeEffect);
+        activeEffect.deps.push(dep);
+    }
+}
+function trigger(target, type, key) {
+    let deps = [];
+    const depsMap = targetMap.get(target);
+    if (!depsMap)
+        return;
+    const dep = depsMap.get(key);
+    deps.push(dep);
+    const effects = [];
+    deps.forEach((dep) => {
+        effects.push(...dep);
+    });
+    triggerEffects(createDep(effects));
+}
+function isTracking() {
+    return shouldTrack && activeEffect !== undefined;
+}
+function triggerEffects(dep) {
+    for (const effect of dep) {
+        if (effect.scheduler) {
+            effect.scheduler();
+        }
+        else {
+            effect.run();
+        }
+    }
+}
+
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+const shallowReadonlyGet = createGetter(true, true);
+function createGetter(isReadonly = false, shallow = false) {
+    return function get(target, key, receiver) {
+        const isExistInReactiveMap = () => key === "__v_raw" && receiver === reactiveMap.get(target);
+        const isExistInReadonlyMap = () => key === "__v_raw" && receiver === readonlyMap.get(target);
+        const isExistInShallowReadonlyMap = () => key === "__v_raw" && receiver === shallowReadonlyMap.get(target);
+        if (key === "__v_isReactive") {
+            return !isReadonly;
+        }
+        else if (key === "__v_isReadonly") {
+            return isReadonly;
+        }
+        else if (isExistInReactiveMap() ||
+            isExistInReadonlyMap() ||
+            isExistInShallowReadonlyMap()) {
+            return target;
+        }
+        const res = Reflect.get(target, key, receiver);
+        if (!isReadonly) {
+            track(target, "get", key);
+        }
+        if (shallow) {
+            return res;
+        }
+        if (isObject(res)) {
+            return isReadonly ? readonly(res) : reactive(res);
+        }
+        return res;
+    };
+}
+function createSetter() {
+    return function set(target, key, value, receiver) {
+        const result = Reflect.set(target, key, value, receiver);
+        trigger(target, "set", key);
+        return result;
+    };
+}
+const readonlyHandlers = {
+    get: readonlyGet,
+    set(target, key) {
+        console.warn(`Set operation on key "${String(key)}" failed: target is readonly.`, target);
+        return true;
+    },
+};
+const mutableHandlers = {
+    get,
+    set,
+};
+const shallowReadonlyHandlers = {
+    get: shallowReadonlyGet,
+    set(target, key) {
+        console.warn(`Set operation on key "${String(key)}" failed: target is readonly.`, target);
+        return true;
+    },
+};
+
+const reactiveMap = new WeakMap();
+const readonlyMap = new WeakMap();
+const shallowReadonlyMap = new WeakMap();
+var ReactiveFlags;
+(function (ReactiveFlags) {
+    ReactiveFlags["IS_REACTIVE"] = "__v_isReactive";
+    ReactiveFlags["IS_READONLY"] = "__v_isReadonly";
+    ReactiveFlags["RAW"] = "__v_raw";
+})(ReactiveFlags || (ReactiveFlags = {}));
+function reactive(target) {
+    return createReactiveObject(target, reactiveMap, mutableHandlers);
+}
+function readonly(target) {
+    return createReactiveObject(target, readonlyMap, readonlyHandlers);
+}
+function shallowReadonly(target) {
+    return createReactiveObject(target, shallowReadonlyMap, shallowReadonlyHandlers);
+}
+function isProxy(value) {
+    return isReactive(value) || isReadonly(value);
+}
+function isReadonly(value) {
+    return !!value["__v_isReadonly"];
+}
+function isReactive(value) {
+    return !!value["__v_isReactive"];
+}
+function createReactiveObject(target, proxyMap, baseHandlers) {
+    const existingProxy = proxyMap.get(target);
+    if (existingProxy) {
+        return existingProxy;
+    }
+    const proxy = new Proxy(target, baseHandlers);
+    proxyMap.set(target, proxy);
+    return proxy;
+}
+
+class RefImpl {
+    constructor(value) {
+        this.__v_isRef = true;
+        this._rawValue = value;
+        this._value = convert(value);
+        this.dep = createDep();
+    }
+    get value() {
+        trackRefValue(this);
+        return this._value;
+    }
+    set value(newValue) {
+        if (hasChanged(newValue, this._rawValue)) {
+            this._value = convert(newValue);
+            this._rawValue = newValue;
+            triggerRefValue(this);
+        }
+    }
+}
+function ref(value) {
+    return createRef(value);
+}
+function convert(value) {
+    return isObject(value) ? reactive(value) : value;
+}
+function createRef(value) {
+    const refImpl = new RefImpl(value);
+    return refImpl;
+}
+function triggerRefValue(ref) {
+    triggerEffects(ref.dep);
+}
+function trackRefValue(ref) {
+    if (isTracking()) {
+        trackEffects(ref.dep);
+    }
+}
+const shallowUnwrapHandlers = {
+    get(target, key, receiver) {
+        return unRef(Reflect.get(target, key, receiver));
+    },
+    set(target, key, value, receiver) {
+        const oldValue = target[key];
+        if (isRef(oldValue) && !isRef(value)) {
+            return (target[key].value = value);
+        }
+        else {
+            return Reflect.set(target, key, value, receiver);
+        }
+    },
+};
+function proxyRefs(objectWithRefs) {
+    return new Proxy(objectWithRefs, shallowUnwrapHandlers);
+}
+function unRef(ref) {
+    return isRef(ref) ? ref.value : ref;
+}
+function isRef(value) {
+    return !!value.__v_isRef;
+}
+
+class ComputedRefImpl {
+    constructor(getter) {
+        this._dirty = true;
+        this.dep = createDep();
+        this.effect = new ReactiveEffect(getter, () => {
+            if (this._dirty)
+                return;
+            this._dirty = true;
+            triggerRefValue(this);
+        });
+    }
+    get value() {
+        trackRefValue(this);
+        if (this._dirty) {
+            this._dirty = false;
+            this._value = this.effect.run();
+        }
+        return this._value;
+    }
+}
+function computed(getter) {
+    return new ComputedRefImpl(getter);
+}
+
+exports.ReactiveEffect = ReactiveEffect;
+exports.computed = computed;
+exports.effect = effect;
+exports.isProxy = isProxy;
+exports.isReactive = isReactive;
+exports.isReadonly = isReadonly;
+exports.isRef = isRef;
+exports.proxyRefs = proxyRefs;
+exports.reactive = reactive;
+exports.readonly = readonly;
+exports.ref = ref;
+exports.shallowReadonly = shallowReadonly;
+exports.stop = stop;
+exports.unRef = unRef;
+//# sourceMappingURL=mini-vue.cjs.js.map
